a59bf289a7d86d36a3611f0c1d79336c5f3fa1af
JONATHAN HAGEN <jhagen920110@gmail.com>
2017-01-29 03:57:14 -0800

    Create interestRate.py

---
+++ interestRate.py
@@ -0,0 +1,19 @@
+# Enter number of years
+numberOfYears = eval(input(
+  "Enter number of years as an integer, e.g., 5: "))
+# Enter interest rate
+interestRate = eval(input("Enter interest rate, e.g., 8.25: "))
+monthlyInterestRate = interestRate/1200
+
+# Enter loan amount
+loanAmount = eval(input("Enter loan amount, e.g., 120000.95: "))
+    
+# Calculate payment
+monthlyPayment = loanAmount * monthlyInterestRate / (1
+  - 1 / (1 + monthlyInterestRate) ** (numberOfYears * 12))
+totalPayment = monthlyPayment * numberOfYears * 12
+
+# Display results
+print("The monthly payment is " +
+  str(int(monthlyPayment * 100) / 100))
+print("The total payment is " + str(int(totalPayment * 100) /100))
